62460d31ec959a1e0b1a89f45aef3025adeeda35
Guilherme Bastos <guiapbastos201881@gmail.com>
2024-11-28 10:53:25 -0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,14 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]');
+document.addEventListener('DOMContentLoaded', function(){ //Aguarda o DOM ser carregado para executar o cógido
+    const buttons = document.querySelectorAll('[data-tab-button]'); //Seleciona todos os botões das abas
+    const questions = document.querySelectorAll('[data-faq-question]'); //Seleciona todas as perguntas do FAQ
 
-    const heroSection = document.querySelector('.hero');
-    const alturaHero =heroSection.clientHeight;
+    const heroSection = document.querySelector('.hero'); //Selciona a seção principal da página, identificada pela classe .hero
+    const alturaHero =heroSection.clientHeight; //Calcula a altura da seção principal para ser usada em funcionalidades relacionadas ao scroll
 
-    window.addEventListener('scroll', function(){
-        const posicaoAtual = window.scrollY;
-
-        if (posicaoAtual > alturaHero){
-            ocultaElementosDoHeader();
+    ocultaElementosDoHeader();                            //Adicona um evento de scroll à janela. Toda vez que o usuário rolar a página, verifica:
+    window.addEventListener('scroll', function(){         //Se o scroll atual ultrapassou a altura da seção principal
+        const posicaoAtual = window.scrollY;              // Caso ultrapasse, o header é ocultado com a função ocultaElementoDoHEader
+        if (posicaoAtual > alturaHero){                   //Caso contrário, o header é exibido com exibeElementosDoHeader
         } else{
             exibeElementosDoHeader();
         }
@@ -30,8 +29,8 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     //Seção FAQ, accordion
-    for (let i = 0; i < questions.length; i++){
-        questions[i].addEventListener('click', abreOuFechaResposta)
+    for (let i = 0; i < questions.length; i++){                           //Percorre todas as perguntas selecionadas em questions
+        questions[i].addEventListener('click', abreOuFechaResposta)       // Adiciona um evento de clique em cada uma, chamando a função abreOuFechaResposta
     }
 })
 
@@ -45,7 +44,7 @@ function exibeElementosDoHeader(){
     header.classList.remove('header--is-hidden');
 }
 
-function abreOuFechaResposta(event){
+function abreOuFechaResposta(event){                      //Identifica o elemento pai da pergunta clicada
     const classe = 'faq__questions__item--is-open';
     const elementoPai = event.target.parentNode;
 
